6d071dcb5aa8230614bc69986a4d9784aabf0bea
Ivan <morgantdarck@gmail.com>
2021-10-19 16:59:36 +0300

    -refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,16 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
+
+    private static final Map<String, String> getenv = System.getenv();
+
+    public static void main(String[] args){
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new Bot("SimpleBot","1733740868:AAEirYMYYJYvw3O3pjHLyn3MlPXWjP_xFGs"));
+            botsApi.registerBot(new Bot(getenv.get("BOT_NAME"), getenv.get("BOT_TOKEN")));
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
